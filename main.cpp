fdd4bba40e5da474cd277fbc95789a34f833f995
vedantapte <vvapte@ucdavis.edu>
2019-03-21 20:57:46 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include "Board.h"
 #include "GameAttributes.h"
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main(int argc, char** argv) {
   //bool playing = true;
   cout << "File: " << argv[1] << endl;
-
+  ifstream in(argv[1]);
+  BattleShip::GameAttributes attributes = BattleShip::GameAttributes(in);
+  BattleShip::Board board = BattleShip::Board(attributes.getNumRows(), attributes.getNumCols());
 
   return 0;
 }
